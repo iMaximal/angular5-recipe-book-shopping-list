e6afaf803bfb2ed4d53c929752e83543721e9548
Maksim Golitsyn <maxim@infofree.ru>
2018-06-18 18:38:05 +0500

    Http Progress

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpRequest } from '@angular/common/http';
 import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
@@ -17,14 +17,26 @@ export class DataStorageService {
   storeRecipes() {
     const token = this.authService.getToken();
 
-    return this.httpClient.put(
+    // return this.httpClient.put(
+    //   `${FIREBASE_DB}/recipes.json`,
+    //   this.recipeService.getRecipes(),
+    //   {
+    //     observe: 'body',
+    //     params: new HttpParams().set('auth', token),
+    //   }
+    // );
+
+    const req = new HttpRequest(
+      'PUT',
       `${FIREBASE_DB}/recipes.json`,
       this.recipeService.getRecipes(),
       {
-        observe: 'body',
+        reportProgress: true,
         params: new HttpParams().set('auth', token),
       }
     );
+
+    return this.httpClient.request(req);
   }
 
   getRecipes() {
