14c6461e699bf74021adfe17e0ada56c562935bc
Maksim Golitsyn <maxim@infofree.ru>
2018-06-19 17:49:05 +0500

    Only React to Actions Once via take(1)

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -41,8 +41,4 @@ export class HeaderComponent implements OnInit {
   onLogout() {
     this.authService.logout();
   }
-
-  // isAuthenticated() {
-  //   return this.authService.isAuthenticated();
-  // }
 }
