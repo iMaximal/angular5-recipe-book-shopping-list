bd2fb55859a1d90e87ff5e3ab5e545f3749fb234
Maksim Golitsyn <maxim@infofree.ru>
2018-06-19 17:38:13 +0500

    Getting State Access in HTTP Interceptor

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -42,7 +42,7 @@ export class HeaderComponent implements OnInit {
     this.authService.logout();
   }
 
-  isAuthenticated() {
-    return this.authService.isAuthenticated();
-  }
+  // isAuthenticated() {
+  //   return this.authService.isAuthenticated();
+  // }
 }
--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import 'rxjs/add/operator/switchMap';
 
-import { AuthService } from '../auth/auth.service';
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercepted', req);
 
-    const copiedReq = req.clone({
-      params: req.params.set('auth', this.authService.getToken())
-    });
-
-    return  next.handle(copiedReq);
+    return this.store.select('auth')
+      .switchMap((authState: fromAuth.State) => {
+        const copiedReq = req.clone({
+          params: req.params.set('auth', authState.token)
+        });
+        return next.handle(copiedReq);
+      });
   }
 }
